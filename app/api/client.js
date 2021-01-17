1a35a650bdf02f9df847f1004192f0a8b5343ab2
James Wu <jwu_nyc@yahoo.com>
2021-01-17 12:45:16 -0500

    messed up the load

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import { create } from 'apisauce'
 
 const apiClient = create.apply({
-    baseURL:"https://pokeapi.co/api/v2/pokemon/?limit=1118",
+    baseURL:"https://pokeapi.co/api/v2/pokemon/?limit=10",
 })
 
 export default apiclient
--- app/screens/PokemonList.js
+++ app/screens/PokemonList.js
@@ -2,42 +2,31 @@ import React, { useState, useEffect } from 'react'
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, } from 'react-native';
 
 import PokemonListItem from '../components/PokemonListItem'
+import getMons from '../api/getMons'
 
-const poke=[
-    {
-        "name": "bulbasaur",
-        "url": "https://pokeapi.co/api/v2/pokemon/1/"
-        },
-        {
-        "name": "ivysaur",
-        "url": "https://pokeapi.co/api/v2/pokemon/2/"
-        },
-        {
-        "name": "venusaur",
-        "url": "https://pokeapi.co/api/v2/pokemon/3/"
-        },
-]
 const PokemonList = ()=>{
     const [ pokemons, setPokemons ] = useState([])
     
-    const mapPokemons = ()=>{
-        pokemons.map(el=>{
-            let { url } = pokemon
-            pokemon.id = url.substring(34, url.length - 1)
-            pokemon.name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-        })
-    }
+    // const mapPokemons = ()=>{
+    //     pokemons.map(el=>{
+    //         let { url } = pokemon
+    //         pokemon.id = url.substring(34, url.length - 1)
+    //         pokemon.name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+    //         return pokemon
+    //     })
+    // }
 
-    useEffect(() => {
-        console.log("this is useeffect")
-        mapPokemons()
-        console.log(pokemons)
-      },[]);
+    // useEffect(() => {
+    //     console.log("this is useeffect")
+    //     mapPokemons()
+    //     console.log(pokemons)
+    //   },[]);
 
-    console.log(pokemons,"what")
+    console.log(mapPokemons(),"this is my consolelog")
     return(
         <SafeAreaView>
             <PokemonListItem image={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'} name='bulba'/>
+            {mapPokemons()}
         </SafeAreaView>
     )
 }
