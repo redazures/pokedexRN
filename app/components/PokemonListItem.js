ab640b09c5f573e051c37dd6b1f6576ee1d94a0f
James Wu <jwu_nyc@yahoo.com>
2021-01-17 11:00:39 -0500

    added gradient button

--- app/components/PokemonListItem.js
+++ app/components/PokemonListItem.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Text } from 'react-native'
 import { ListItem, Avatar } from 'react-native-elements'
+import TouchableScale from 'react-native-touchable-scale'; // https://github.com/kohver/react-native-touchable-scale
+import LinearGradient from 'react-native-linear-gradient'; // Only if no expo
 
 const PokemonListItem = ({ image, name })=>(
-  <ListItem bottomDivider>
-    <Avatar source={{uri: image}} />
+  <ListItem 
+    rounded 
+    bottomDivider
+    Component={TouchableScale}
+    friction={90}
+    tension={100}
+    activeScale={0.95}
+    linearGradientProps={{
+      colors: ['red', 'blue'],
+      start: { x: 1, y: 0 },
+      end: { x: 0.2, y: 0 },
+    }}
+    ViewComponent={LinearGradient}
+  >
+    <Avatar source={{uri: image}} size={"large"}/>
     <ListItem.Content>
-      <ListItem.Title>{name}</ListItem.Title>
+      <ListItem.Title style={{ color: 'yellow', fontWeight: 'bold', fontSize:20 }}>{name}</ListItem.Title>
     </ListItem.Content>
   </ListItem>
 )
